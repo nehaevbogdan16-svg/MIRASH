c8409bd040aee93f67cc13973ced0a1652ffd090
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2026-03-30 15:09:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const clans = [
             { name: "ツGU×Margosha", avatar: "img/margo.png", tag: "@ofiwxlz", rank: "Алмаз", role: "Заместитель", roleClass: "leader-role" },
             { name: "ツGU×Ｐｒｅｓ", avatar: "img/vlad.png", tag: "@Likorinenko", rank: "Алмаз", role: "Заместитель", roleClass: "zam-role" },
             { name: "ツGU×ГАЗ", avatar: "img/GAS.png", tag: "UID: 5438651452", rank: "Алмаз", role: "Член клана", roleClass: "member-role" },
-           { name: "cys DRANNIK", avatar: "img/vana.png", tag: "UID: 5647598839", rank: "Платина", role: "Член клана", roleClass: "member-role" }
+           { name: "ツGU×DRANNIK", avatar: "img/vana.png", tag: "UID: 5647598839", rank: "Платина", role: "Член клана", roleClass: "member-role" }
         ],
         top: ["🥇 Bogdan", "🥈 Margosha", "🥉 Pres"]
     },
