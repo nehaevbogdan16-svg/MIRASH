b7988f2025af9dbf3c61854414a2e32a6f7c19f3
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2026-06-23 03:48:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,58 @@
 const clans = [
     {
         id: "mirash",
-        name: "2PIK FAMILY",
+        name: "2PIK",
         avatar: "img/2PIK.png",
         logo: "img/2PIK.png",
         level: 9,
-        membersCount: "22/60",
+        membersCount: "16/60",
         main: true,
         leaders: [
             { name: "2PIK丨GRESHNCA", avatar: "img/margo.png", role: "Заместитель", roleClass: "leader-role", uid: "51514497454" },
             { name: "2PIK丨GRESHNIK", avatar: "img/bogdan.png", role: "Лидер", roleClass: "leader-role", main: true },
+            { name: "2PIK丨SCAM", avatar: "img/SCAM.png", role: "Заместитель", roleClass: "zam-role", uid: "51655879673" },
+            { name: "2PIK丨SPEED", avatar: "img/SPEED.png", role: "Заместитель", roleClass: "zam-role", uid: "51900179636" },
             { name: "2PIK丨Ｌｉｎｋ", avatar: "img/vlad.png", role: "Заместитель", roleClass: "zam-role", uid: "5958320567" }
         ],
         members: [
-            { name: "2PIK丨GRESHNIK", avatar: "img/bogdan.png", tag: "ТГ: @Bogdan_10_07_24", rank: "Корона", role: "Лидер", roleClass: "leader-role" },
-            { name: "2PIK丨GRESHNCA", avatar: "img/margo.png", tag: "ТГ: @ofiwxlz", rank: "Корона", role: "Заместитель", roleClass: "leader-role" },
-            { name: "2PIK丨Ｌｉｎｋ", avatar: "img/vlad.png", tag: "ТГ: @Likorinenko", rank: "Корона", role: "Заместитель", roleClass: "zam-role" },
-            { name: "2PIK丨SCAM", avatar: "img/SCAM.png", tag: "ТГ: @@Solo_Leveling_666", tag: "UID: 5438651452", rank: "Корона", role: "Заместитель", roleClass: "member-role" },
-           { name: "2PIK丨SPEED", avatar: "img/SPEED.png", tag: "UID: ...", rank: "...", role: "Элита", roleClass: "member-role" }
+           // ===================== t =====================
+            { name: "2PIK丨GRESHNIK", avatar: "img/bogdan.png", tag: "ТГ: @no2PIK", rank: "", role: "Лидер", roleClass: "leader-role" },
+            { name: "2PIK丨GRESHNCA", avatar: "img/margo.png", tag: "ТГ: @ofiwxlz", rank: "", role: "Заместитель", roleClass: "leader-role" },
+            { name: "2PIK丨Ｌｉｎｋ", avatar: "img/vlad.png", tag: "ТГ: @Likorinenko", rank: "", role: "Заместитель", roleClass: "zam-role" },
+            { name: "2PIK丨SCAM", avatar: "img/SCAM.png", tag: "ТГ: @Solo_Leveling_666", tag: "UID: 5438651452", rank: "", role: "zam-role", roleClass: "member-role" },
+            { name: "2PIK丨SPEED", avatar: "img/SPEED.png", tag: "UID: 51900179636", rank: "", role: "Элита", roleClass: "zam-role" },
+            // ===================== ДАННЫЕ КЛАНОВ =====================
+            { name: "2PIK丨RICH", avatar: "img/RICH.png", tag: "UID: 51859443069", rank: "", role: "Элита", roleClass: "member-role" },
+            { name: "2PIK丨RAIL", avatar: "img/RAIL.png", tag: "UID: 52291257528", rank: "", role: "Элита", roleClass: "member-role" },
+            { name: "2PIK丨ARGONAFT", avatar: "img/ARGONAFT.png", tag: "UID: 5796280595", rank: "", role: "Элита", roleClass: "member-role" },
+            { name: "2PIK丨AZART", avatar: "img/AZART.png", tag: "UID: 5939911678", rank: "", role: "Элита", roleClass: "member-role" },
+            { name: "2PIK丨FoWeska7", avatar: "img/FoWeska7.png", tag: "UID: 5413564659", rank: "", role: "Элита", roleClass: "member-role" },
+            // ===================== ДАННЫЕ КЛАНОВ =====================
+            { name: "2PIK丨stalik", avatar: "img/stalik.png", tag: "UID: 5717071471", rank: "", role: "Член клана", roleClass: "member-role" },
+            { name: "2PIK丨ARES", avatar: "img/ARES.png", tag: "UID: 51468324125", rank: "", role: "Член клана", roleClass: "member-role" },
+            { name: "2PIK丨Haizen", avatar: "img/Haizen.png", tag: "UID: 52216804594", rank: "", role: "Член клана", roleClass: "member-role" },
+            { name: "2PIK丨Miyabi", avatar: "img/Miyabi.png", tag: "UID: 51893217266", rank: "", role: "Член клана", roleClass: "member-role" },
+            { name: "2PIK丨PIZDUK", avatar: "img/PIZDUK.png", tag: "UID: 52295397626", rank: "", role: "Член клана", roleClass: "member-role" },
+            { name: "2PIK丨doup", avatar: "img/doup.png", tag: "UID: 5400577570", rank: "", role: "Член клана", roleClass: "member-role" },
         ],
-        top: ["🥇 2PIK丨GRESHNIK", "🥈 2PIK丨GRESHNCA", "🥉 2PIK丨Ｌｉｎｋ", "4 2PIK丨SCAM"]
+        top: ["🥇 2PIK丨GRESHNIK", "🥈 2PIK丨GRESHNCA", "🥉 2PIK丨SCAM", "4 2PIK丨Ｌｉｎｋ"]
     },
     { id: "storm", 
-     name: "ツGU×STORM", 
+     name: "2PIK STORM", 
      avatar: "img/clan2.png", 
      logo: "img/clan2.png", 
      level: 1, 
      membersCount: "2/30", 
      main: false, 
      leaders: [
-         { name: "ツGU×FENRIR", uid: "51619988168", avatar: "img/FENRIR.png", role: "Лидер", roleClass: "leader-role", main: true },
+         { name: "2PIK丨FENRIR", uid: "51619988168", avatar: "img/FENRIR.png", role: "Лидер", roleClass: "leader-role", main: true },
      ], 
      members: [
-         { name: "ツGU×FENRIR", avatar: "img/FENRIR.png", tag: "ТГ: @God_FENRIR", rank: "51619988168", role: "Лидер", roleClass: "leader-role" },
-         { name: "ツGU×Girl", avatar: "img/ксения.png", rank: "52453042310", role: "Член клана", roleClass: "member-role" }
+         { name: "2PIK丨FENRIR", avatar: "img/FENRIR.png", tag: "ТГ: @God_FENRIR", rank: "51619988168", role: "Лидер", roleClass: "leader-role" },
+         { name: "2PIK丨Шут", avatar: "img/Шут.png", rank: "52453042310", role: "Заместитель", roleClass: "zam-role" },
+         { name: "", avatar: "img/.png", tag: "UID: ", rank: "", role: "", roleClass: "" },
      ], 
-     top: ["🥇 ツGU×FENRIR", "ツGU×Girl"] },
+     top: ["🥇 2PIK丨FENRIR", "🥈 2PIK丨Шут"] },
     
     { id: "ghost", 
      name: "Свободное место", 
